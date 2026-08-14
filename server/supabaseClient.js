ede16083285eb9eb196ddf60d11b78adf41a5541
Nosewise0 <nonsaker021@gmail.com>
2026-08-14 21:46:22 +0800

    remove unused files

--- server/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const supaBase = require("@supabase/supabase-js");
-
-const supabase = supaBase.createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);  
-
-module.exports = supabase;
